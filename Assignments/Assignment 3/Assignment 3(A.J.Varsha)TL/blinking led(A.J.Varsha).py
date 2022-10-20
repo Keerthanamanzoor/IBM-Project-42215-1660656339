d543452f62a2bf78b1ebf3756a7589ceac85c33b
AJ-Varsha <113257377+AJ-Varsha@users.noreply.github.com>
2022-10-20 18:39:59 +0530

    Update blinking led(A.J.Varsha).py

--- Assignments/Assignment 3/Assignment 3(A.J.Varsha)TL/blinking led(A.J.Varsha).py
+++ Assignments/Assignment 3/Assignment 3(A.J.Varsha)TL/blinking led(A.J.Varsha).py
@@ -1,38 +1,20 @@
 import RPi.GPIO as GPIO
 import time
-
-try:
-def lightTraffic(led1,led2,led3,delay)
-GPIO.output(led1,1)
-time.sleep(delay)
-GPIO.output(led1,0)
-GPIO.output(led2,1)
-time.sleep(delay)
-GPIO.output(led2,0)
-GPIO.output(led3,1)
-time.sleep(delay)
-GPIO.output(led3,0)
-GPIO.setmode(GPIO,BCM)
-button=19
-GPIO.setup(button,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-ledGreen=16
-ledYellow=12
-ledRed=23
-GPIO.setup(ledGreen,GPIO.OUT)
-GPIO.setup(ledYellow,GPIO.OUT)
-GPIO.setup(ledRed,GPIO.OUT)
-While True:
-	input_state=GPIO.input(button)
-	if input_state==False
-		Print(“Button Pressed”)
-		LightTrafic(ledGreen,ledYellow,ledRed,1)
-	else:
-		GPIO.output(ledGreen,0)
-		GPIO.output(ledYellow,0)
-		GPIO.output(ledRed,0)
-
-except KeyboardInterrupt
-	Print
-	“You have exited the program”
-finally:
-	GPIO.cleanup()
+#assign number for the GPIO using BCM
+GPIO.setmode (GPIO.BCM)
+#assign number for the GPIO using Board
+GPIO.setmode(GPIO.BOARD)
+cnt=0
+MAIL_CHECK_FREQ=1
+#change LED status every 1 seconds
+RED_LED=4
+GPIO.setup(RED_LED,GPIO.OUT)
+while True
+if cnt==0:
+GPIO.output(RED_LED,False)
+cnt=1
+else:
+GPIO.output(RED_LED,True)
+cnt=0:
+time.sleep(MAIL_CHECK_FREQ)
+GPIO.cleanup()
